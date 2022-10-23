07d68f5feb0d1a6a20eb4eda11df430cd17915e3
chitkiu <>
2022-10-23 03:07:31 +0300

    Code polishing

--- lib/download_station/create_task/select_destination_screen.dart
+++ lib/download_station/create_task/select_destination_screen.dart
@@ -1,6 +1,7 @@
+// import 'package:animated_tree_view/animated_tree_view.dart';
 import 'package:dsm_sdk/file_station/fs_file_info_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_tree/flutter_tree.dart';
+import 'package:flutter_fancy_tree_view/flutter_fancy_tree_view.dart';
 
 import '../../sdk.dart';
 
@@ -11,46 +12,85 @@ class SelectDestinationWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var data = models.where((element) => element.isDir).map((e) => TreeNodeData(
-          title: e.name,
-          checked: false,
-          expaned: false,
-          extra: e,
-          children: List.empty(),
-        ));
+    var root = TreeNode(id: "Root");
+    root.addChildren(
+        models.where((element) => element.isDir).map((e) => TreeNode(
+              id: e.path,
+              data: e,
+            )));
+    var controller = TreeViewController(rootNode: root);
     return AlertDialog(
       title: const Text("Select directory on server"),
-      content: TreeView(
-          data: data.toList(),
-          lazy: true,
-          showCheckBox: true,
-          load: _load,
-          onCheck: (checked, node) {
-            var data = node.extra;
-            if (data is FSFileInfoModel) {
-              Navigator.pop(context, data);
-            }
-          }),
+      content: SizedBox(
+        height: 500,
+        width: 400,
+        child: TreeView(
+          controller: controller,
+          nodeBuilder: (context, node) {
+            return const NodeTreeView();
+          },
+        ),
+      ),
     );
   }
+}
 
-  Future<List<TreeNodeData>> _load(TreeNodeData parent) async {
-    var extra = parent.extra;
-    if (extra is FSFileInfoModel) {
-      var newDirs = await SDK.instance.sdk.fsSDK.getFolderList(extra.path);
-      if (newDirs.isSuccess) {
-        return newDirs.successValue
-            .where((element) => element.isDir)
-            .map((e) => TreeNodeData(
-                  title: e.name,
-                  checked: false,
-                  expaned: false,
-                  extra: e,
-                  children: List.empty(),
-                ))
-            .toList();
-      }
+class NodeTreeView extends StatelessWidget {
+  const NodeTreeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var scope = TreeNodeScope.of(context);
+    var data = scope.node.data;
+    if (data is FSFileInfoModel) {
+      return InkWell(
+          onTap: () async {
+            if (scope.isExpanded) {
+              scope.collapse(context);
+            } else {
+              var newDirs =
+                  await SDK.instance.sdk.fsSDK.getFolderList(data.path);
+              if (newDirs.isSuccess) {
+                var items = newDirs.successValue
+                    .where((element) => element.isDir)
+                    .map((e) => TreeNode(
+                          id: e.path,
+                          data: e,
+                        ))
+                    .toList();
+                scope.node.addChildren(items);
+                scope.expand(context);
+              }
+            }
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: scope.indentation,
+                    height: double.infinity,
+                  ),
+                  const ExpandNodeIcon(
+                    padding: EdgeInsets.zero,
+                    size: 20,
+                  ),
+                  Expanded(
+                      child: Text(
+                    data.name,
+                    overflow: TextOverflow.ellipsis,
+                  )),
+                  TextButton(
+                    child: const Text("Select"),
+                    onPressed: () {
+                      Navigator.pop(context, data);
+                    },
+                  ),
+                ]),
+          ));
+    } else {
+      return Container();
     }
-    return List.empty();
   }
 }
