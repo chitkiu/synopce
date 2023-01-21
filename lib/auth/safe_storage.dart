be7fd5825c1ab4e4026746b5b2ee9d2ff76e11a5
chitkiu <>
2023-01-21 22:28:13 +0200

    Deleted logging. Updated dependencies

--- lib/auth/safe_storage.dart
+++ lib/auth/safe_storage.dart
@@ -23,13 +23,11 @@ class SafeStorage implements Storage {
 
   @override
   Future<void> delete(String key) {
-    print('SafeStorage delete key $key');
     return _storage.delete(key: STORAGE_PREFIX+key);
   }
 
   @override
   Future<void> deleteAll(List<String> keys) async {
-    print('SafeStorage deleteAll keys $keys');
     for (var element in keys) {
       await delete(element);
     }
@@ -37,19 +35,16 @@ class SafeStorage implements Storage {
 
   @override
   Future<void> init(bool persistSession, bool ignoreExpires) async {
-    print('SafeStorage init ${await readAll()}');
   }
 
   @override
   Future<String?> read(String key) async {
     var response = await _storage.read(key: STORAGE_PREFIX+key);
-    print('SafeStorage read key $key response $response');
     return response;
   }
 
   @override
   Future<void> write(String key, String value) {
-    print('SafeStorage write key $key value $value');
     return _storage.write(
       key: STORAGE_PREFIX+key,
       value: value,
