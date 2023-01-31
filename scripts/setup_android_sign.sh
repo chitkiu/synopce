8d7cc8021c68701cd7da5c96efade3190b322f69
chitkiu <>
2023-02-01 01:17:31 +0200

    Try to fix android app build

--- scripts/setup_android_sign.sh
+++ scripts/setup_android_sign.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-cat ${ANDROID_KEYSTORE}
-echo ${ANDROID_KEYSTORE}
-echo -n ${ANDROID_KEYSTORE} | base64 -d > android/my.jks
+#cat ${ANDROID_KEYSTORE}
+#echo ${ANDROID_KEYSTORE}
+cat ${ANDROID_KEYSTORE} | base64 -d > android/my.jks
+cat android/my.jks
 echo "storePassword=${ANDROID_KEYSTORE_PASSWORD}" >> android/signing.properties
 echo "keyPassword=${ANDROID_KEY_PASSWORD}" >> android/signing.properties
 echo "keyAlias=${ANDROID_KEYSTORE_ALIAS}" >> android/signing.properties
